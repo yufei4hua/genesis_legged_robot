da31f1b23039d76516a24a2656cd711ee9286d1f
yufei4hua <michael4hua@gmail.com>
2025-07-28 15:57:30 +0200

    feat: add resume from checkpoint function

--- src/ainex_train.py
+++ src/ainex_train.py
@@ -40,7 +40,7 @@ def get_train_cfg(exp_name, max_iterations):
             "load_run": -1,
             "log_interval": 1,
             "max_iterations": max_iterations,
-            "num_steps_per_env": 48, #48, #24,
+            "num_steps_per_env": 192, #48, #24,
             "policy_class_name": "ActorCritic",
             "record_interval": -1,
             "resume": False,
@@ -98,7 +98,7 @@ def get_cfgs():
         "num_obs": 61*5,
         "num_privileged_obs": 64*5,
         "obs_scales": {
-            "lin_vel": 10.0,
+            "lin_vel": 2.0,
             "ang_vel": 0.25,
             "dof_pos": 1.0,
             "dof_vel": 0.05,
@@ -124,14 +124,14 @@ def get_cfgs():
             "feet_distance": 0.01, #0.2,
             "knee_distance": 0.05, #0.2,
                         
-            "tracking_lin_vel": 0.4, #1.5, 
-            "tracking_ang_vel": 0.1, #1.0,
+            "tracking_lin_vel": 0.3, #1.5, 
+            "tracking_ang_vel": 0.2, #1.0,
             "vel_mismatch_exp": 0.02, #0.5,
-            "low_speed": 0.1,
+            "low_speed": 0.02,
             "track_vel_hard": 0, #0.5,
             
             "default_joint_pos": 0.01,
-            "orientation": 0.1, #1.0,
+            "orientation": 0.01, #1.0,
             "base_height": 0.15, #0.2,
             
             "base_acc": 0.01, #0.2,
@@ -168,6 +168,7 @@ def main():
     parser.add_argument("--max_iterations", type=int, default=500)
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--headless", action="store_true", default=False)
+    parser.add_argument("--ckpt", type=int, default=0)
     args = parser.parse_args()
 
     gs.init(logging_level="warning")
@@ -177,7 +178,8 @@ def main():
     train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
 
     if os.path.exists(log_dir):
-        shutil.rmtree(log_dir)
+        # shutil.rmtree(log_dir) # do not purge directory
+        pass
     os.makedirs(log_dir, exist_ok=True)
 
     env = AinexEnv(
@@ -190,6 +192,11 @@ def main():
     )
 
     runner = OnPolicyRunner(env, train_cfg, log_dir, device="cuda:0")
+
+    # resume from checkpoint
+    if args.ckpt > 0:
+        resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
+        runner.load(resume_path)
 
     pickle.dump(
         [env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg],
